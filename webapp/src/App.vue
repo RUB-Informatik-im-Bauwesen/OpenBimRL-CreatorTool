79d0840ac4a63fe5e8fd14226bf78bf6975f8137
Florian Becker <florian00m.becker@gmail.com>
2023-04-16 23:47:51 +0000

    recreated project with vite + vue3 + ts + vueflow

---
+++ webapp/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Flowchart from './components/Flowchart.vue'
+</script>
+
+<template>
+  <Flowchart style="height: 90vh;" />
+</template>
+
+<style scoped>
+</style>
---
+++ webapp/src/components/Flowchart.vue
@@ -0,0 +1,27 @@
+<!-- Flowchart.vue -->
+<script lang="ts" setup>
+import { VueFlow } from '@vue-flow/core'
+import { ref } from 'vue';
+
+const elements = ref([
+  {
+    id: '1',
+    label: 'node 1',
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '2',
+    label: 'node 2',
+    position: { x: 100, y: 200 },
+  },
+  {
+    id: 'e1-2',
+    target: '2',
+    source: '1',
+  },
+])
+</script>
+
+<template>
+  <VueFlow v-model="elements"></VueFlow>
+</template>
