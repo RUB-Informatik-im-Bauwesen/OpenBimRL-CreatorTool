3e61156a817ecb497cd56e0716bd5b2355b81abc
Florian Becker <florian00m.becker@gmail.com>
2023-04-17 00:20:48 +0000

    added navigation template

---
+++ webapp/src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>
+    <ul>
+      <li><a>New</a></li>
+      <li>
+        <div>
+          <a>Upload</a>
+          <a>Download</a>
+        </div>
+      </li>
+      <li><a>Create Nodes</a></li>
+      <li><a>Help?</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped></style>
