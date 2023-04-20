d0639541e73a2b1149cfc5d8e00eff79808aa50b
Florian Becker <florian00m.becker@gmail.com>
2023-04-20 16:27:25 +0000

    added side overlay

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Flowchart from "./components/Flowchart.vue";
+import SideOverlay from "./components/SideOverlay.vue";
 import Navigation from "./components/navigation";
 </script>
 
 <template>
   <Navigation />
+  <SideOverlay />
   <Flowchart class="h-screen" />
 </template>
 
