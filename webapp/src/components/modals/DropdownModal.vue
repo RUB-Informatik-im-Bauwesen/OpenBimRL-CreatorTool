2dd2762b34f78fdaec07da639403ba3e13ce431a
Florian Becker <florian00m.becker@gmail.com>
2023-04-20 15:19:35 +0000

    added dynamic modal elements

--- webapp/src/components/modals/DropdownModal.vue
+++ webapp/src/components/modals/DropdownModal.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="anchor">
     <dialog
-      class="absolute block h-20 w-full bg-red-800 -translate-x-full top-8"
+      class="absolute block bg-white shadow-lg -translate-x-full top-8"
       :class="{ hidden: !active }"
-    ></dialog>
+    >
+    <ul class="flex flex-col gap-px">
+      <li v-for="item in Object.keys(modalItems)" class="border">
+        <a @click.stop="modalItems[item]" class="cursor-pointer px-4">{{ item }}</a>
+      </li>
+    </ul>
+  </dialog>
   </div>
 </template>
 
 <script setup lang="ts">
 import { registerModal } from ".";
+import { DropdownProps } from "../navigation/Types";
+
+const { modalItems } = defineProps<{ modalItems: DropdownProps }>();
 
 const active = registerModal();
 defineExpose({ active });
--- webapp/src/components/navigation/Navigation.vue
+++ webapp/src/components/navigation/Navigation.vue
@@ -1,29 +1,45 @@
 <script setup lang="ts">
 import Dropdown from "./Dropdown.vue";
+import { DropdownProps } from "./Types";
+
+const uploadModalItems = {
+    XML: () => console.log("XML"),
+    JSON: () => console.log("JSON"),
+};
+const downloadModalItems = {
+    XML: () => console.log("XML"),
+    JSON: () => console.log("JSON"),
+};
 </script>
 
 <template>
-  <nav class="w-full fixed p-1 pl-8 bg-gray-300 text-white z-10">
-    <ul class="flex gap-4 mt-1">
-      <li><button class="nav-button">New</button></li>
-      <li>
-        <div class="flex gap-1">
-          <Dropdown>Upload</Dropdown>
-          <Dropdown>Download</Dropdown>
-        </div>
-      </li>
-      <li><button class="blue bg-blue-600 nav-button">Create Nodes</button></li>
-      <li><button class="nav-button">Help?</button></li>
-    </ul>
-  </nav>
+    <nav class="w-full fixed p-1 pl-8 bg-gray-300 text-white z-10">
+        <ul class="flex gap-4 mt-1">
+            <li><button class="nav-button">New</button></li>
+            <li>
+                <div class="flex gap-1">
+                    <Dropdown :modal-items="uploadModalItems">Upload</Dropdown>
+                    <Dropdown :modal-items="downloadModalItems"
+                        >Download</Dropdown
+                    >
+                </div>
+            </li>
+            <li>
+                <button class="blue bg-blue-600 nav-button">
+                    Create Nodes
+                </button>
+            </li>
+            <li><button class="nav-button">Help?</button></li>
+        </ul>
+    </nav>
 </template>
 
 <style>
 button.nav-button {
-  @apply rounded-sm py-1 px-2;
+    @apply rounded-sm py-1 px-2;
 }
 
 button.nav-button:not(.blue) {
-  @apply bg-gray-500;
+    @apply bg-gray-500;
 }
 </style>
