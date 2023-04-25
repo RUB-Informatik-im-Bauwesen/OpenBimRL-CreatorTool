992fc5e593bae6fab0cac910bc207b68e8a6a7da
Florian Becker <florian00m.becker@gmail.com>
2023-04-25 15:30:51 +0000

    added components/index.ts and added help to main

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-import Flowchart from "./components/Flowchart.vue";
-import SideOverlay from "./components/SideOverlay.vue";
+import { ref } from "vue";
+import { Flowchart, Help, SideOverlay } from "./components";
 import Navigation from "./components/navigation";
+
+const showHelp = ref(false);
 </script>
 
 <template>
-  <Navigation />
-  <SideOverlay />
-  <Flowchart class="h-screen" />
+    <Navigation @showHelp="showHelp = !showHelp" />
+    <SideOverlay />
+    <Help v-show="showHelp" @close="showHelp = false" />
+    <Flowchart class="h-screen" />
 </template>
 
 <style scoped></style>
---
+++ webapp/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Help } from "./Help.vue";
+export { default as SideOverlay } from "./SideOverlay.vue";
+export { default as Flowchart } from "./Flowchart.vue";
