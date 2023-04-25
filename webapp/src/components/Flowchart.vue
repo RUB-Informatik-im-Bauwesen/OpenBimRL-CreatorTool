4984133646e0d57ae9d807964bc55c04afdbfb07
Florian Becker <florian00m.becker@gmail.com>
2023-04-25 15:55:33 +0000

    added vue-flow background

--- webapp/src/components/Flowchart.vue
+++ webapp/src/components/Flowchart.vue
@@ -1,27 +1,30 @@
 <!-- Flowchart.vue -->
 <script lang="ts" setup>
 import { VueFlow } from "@vue-flow/core";
+import { Background, BackgroundVariant } from '@vue-flow/background'
 import { ref } from "vue";
 
 const elements = ref([
-  {
-    id: "1",
-    label: "node 1",
-    position: { x: 100, y: 100 },
-  },
-  {
-    id: "2",
-    label: "node 2",
-    position: { x: 100, y: 200 },
-  },
-  {
-    id: "e1-2",
-    target: "2",
-    source: "1",
-  },
+    {
+        id: "1",
+        label: "node 1",
+        position: { x: 100, y: 100 },
+    },
+    {
+        id: "2",
+        label: "node 2",
+        position: { x: 100, y: 200 },
+    },
+    {
+        id: "e1-2",
+        target: "2",
+        source: "1",
+    },
 ]);
 </script>
 
 <template>
-  <VueFlow v-model="elements"></VueFlow>
+    <VueFlow v-model="elements">
+        <Background :variant="BackgroundVariant.Lines" :pattern-color="'#efefef'" :size="0.8" />
+    </VueFlow>
 </template>
