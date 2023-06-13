87bce15b57ffcfa88c743c7b86c4863ddd3d06eb
Florian <Florian.Becker-f86@ruhr-uni-bochum.de>
2023-06-13 13:18:52 +0200

    optimized for the use of windows

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,12 +1,12 @@
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath,URL } from 'url';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     server: {
-        port: 3000,
+        port: 8000,
     },
     resolve: {
         alias: {
